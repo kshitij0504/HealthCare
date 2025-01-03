836e666367a15d3f112fb3c214afbec0e886c81a
KshitijOza <121845519+kshitij0504@users.noreply.github.com>
2025-01-03 13:12:40 +0530

    Update user.controller.js

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -69,7 +69,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true, 
+       secure: process.env.NODE_ENV === "production", 
       sameSite: "strict",
       maxAge: 3600000, // 1 hour
     });
